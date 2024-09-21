4cc77e2c09014e80965bb564f8103cecf93692ea
N1tn1 <155231688+N1tn1@users.noreply.github.com>
2024-09-21 05:18:15 -0500

    Assignment for Week14

--- app.js
+++ app.js
@@ -36,28 +36,22 @@ app.use(session(sessionParms));
 
 app.use(require("connect-flash")());
 
-app.get("/secretWord", (req, res) => {
-    if (!req.session.secretWord) {
-        req.session.secretWord = "syzygy";
-    }
-    res.locals.info = req.flash("info");
-    res.locals.errors = req.flash("error");
-    res.render("secretWord", {
-        secretWord,
-        errors: flash("error"),
-        info: flash("info"),
-    });
+app.use(require("./middleware/storeLocals"));
+app.get("/", (req, res) => {
+    res.render("index");
 });
-app.post("/secretWord", (req, res) => {
-    if (req.body.secretWord.toUpperCase()[0] == "P") {
-        req.flash("error", "That word won't work!");
-        req.flash("error", "You can't use words that start with p.");
-    } else {
-        req.session.secretWord = req.body.secretWord;
-        req.flash("info", "The secret word was changed.");
-    }
-    res.redirect("/secretWord");
-});
+app.use("/sessions", require("./routes/sessionRoutes"));
+
+const passport = require("passport");
+const passportInit = require("./passport/passportInit");
+
+passportInit();
+app.use(passport.initialize());
+app.use(passport.session());
+
+const auth = require("./middleware/auth");
+const secretWordRouter = require("./routes/secretWord");
+app.use("/secretWord", auth, secretWordRouter);
 
 app.use((req, res) => {
     res.status(404).send(`That page (${req.url}) was not found.`);
@@ -72,6 +66,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
+        await require("./db/connect")(process.env.MONGO_URI);
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
